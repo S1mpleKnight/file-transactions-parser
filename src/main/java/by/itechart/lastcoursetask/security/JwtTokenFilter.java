2360b4657dc01e9bce9e70fa542c48fe98e7826d
S1mpleKnight <become.adult77@gmail.com>
2021-08-27 01:01:05 +0300

    fix: change parent class(JwtTokenFilter)

--- src/main/java/by/itechart/lastcoursetask/security/JwtTokenFilter.java
+++ src/main/java/by/itechart/lastcoursetask/security/JwtTokenFilter.java
@@ -6,12 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,18 +17,18 @@ import java.io.IOException;
 @Component
 @Slf4j
 @AllArgsConstructor
-public class JwtTokenFilter extends GenericFilterBean {
+public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtTokenProvider tokenProvider;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String token = tokenProvider.resolveToken((HttpServletRequest) servletRequest);
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = tokenProvider.resolveToken(httpServletRequest);
         try {
             acceptAuthentication(token);
         } catch (JwtAuthenticationException e) {
-            rejectAuthentication((HttpServletResponse) servletResponse, e);
+            rejectAuthentication( httpServletResponse, e);
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
     private void rejectAuthentication(HttpServletResponse servletResponse, JwtAuthenticationException e) throws IOException {
