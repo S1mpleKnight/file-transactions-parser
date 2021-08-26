ac2027e9f50e283086e8abf8d0abacf84e3cdc28
S1mpleKnight <become.adult77@gmail.com>
2021-08-26 16:51:11 +0300

    fix: correct db script

--- src/main/resources/db/migration/V1__Init_database.sql
+++ src/main/resources/db/migration/V1__Init_database.sql
@@ -1,19 +1,19 @@
 create table role
 (
-    id      int(11) not null auto_increment
+    id      int not null auto_increment
         primary key,
     name    varchar(5)  not null
 );
 
 create table operator
 (
-    id         int(11) not null auto_increment
+    id         int not null auto_increment
         primary key,
     first_name varchar(20) not null,
     last_name  varchar(30) not null,
     nickname   varchar(15) not null unique,
     password   varchar(50) not null,
-    role_id    int(11) not null,
+    role_id    int not null,
     constraint operator_role_id_fk
         foreign key (role_id) references role (id)
             on update cascade on delete cascade
@@ -28,7 +28,7 @@ create table transactions
     amount              bigint       not null,
     customer_id         varchar(36)  not null,
     date_time           timestamp    not null,
-    operator_id         int(11)      null,
+    operator_id         int     null,
     constraint transactions_operator_id_fk
         foreign key (operator_id) references operator (id)
            on update cascade on delete set null
