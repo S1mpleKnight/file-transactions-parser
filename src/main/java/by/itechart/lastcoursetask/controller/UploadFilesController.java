37070df8d1b905b3e27fdd4cd3b28eed6362be26
S1mpleKnight <become.adult77@gmail.com>
2021-08-28 15:32:04 +0300

    fix: change preauthorize method(hasAuthority)

--- src/main/java/by/itechart/lastcoursetask/controller/UploadFilesController.java
+++ src/main/java/by/itechart/lastcoursetask/controller/UploadFilesController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/files")
-@PreAuthorize("hasRole('user')")
+@PreAuthorize("hasAuthority('USER')")
 public class UploadFilesController {
     private final FileTransferService transferService;
 
