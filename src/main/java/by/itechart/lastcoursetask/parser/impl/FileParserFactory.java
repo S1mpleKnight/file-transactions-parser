c14f6b7140e8d80828c573b8fb6908f24b1b282b
S1mpleKnight <become.adult77@gmail.com>
2021-08-30 14:44:44 +0300

    fix: change file factory scope

--- src/main/java/by/itechart/lastcoursetask/parser/impl/FileParserFactory.java
+++ src/main/java/by/itechart/lastcoursetask/parser/impl/FileParserFactory.java
@@ -3,12 +3,14 @@ package by.itechart.lastcoursetask.parser.impl;
 import by.itechart.lastcoursetask.exception.InvalidFileExtensionException;
 import by.itechart.lastcoursetask.parser.api.FileParser;
 import lombok.Data;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 @Data
+@Scope("singleton")
 public class FileParserFactory {
     private final Map<String, FileParser> parserMap;
 
