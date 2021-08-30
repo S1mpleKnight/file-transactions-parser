2114acdef2bafce086a43a4a0cbc66296ce0c315
S1mpleKnight <become.adult77@gmail.com>
2021-08-30 14:45:22 +0300

    fix: implement comparable in TransactionDto

--- src/main/java/by/itechart/lastcoursetask/dto/TransactionDto.java
+++ src/main/java/by/itechart/lastcoursetask/dto/TransactionDto.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TransactionDto {
+public class TransactionDto implements Comparable<TransactionDto>{
     private String transactionId;
     private String customerId;
     private String currency;
     private String amount;
     private Boolean status;
     private String dateTime;
+
+    @Override
+    public int compareTo(TransactionDto o) {
+        return new BigDecimal(this.amount).compareTo(new BigDecimal(o.getAmount()));
+    }
 }
