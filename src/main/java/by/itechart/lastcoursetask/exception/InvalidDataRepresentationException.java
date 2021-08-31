f4ee822d9d8f47cee71e21c666171dfbb855ab66
S1mpleKnight <become.adult77@gmail.com>
2021-08-31 16:32:05 +0300

    delete: remove InvalidDataRepresentationException

--- src/main/java/by/itechart/lastcoursetask/exception/InvalidDataRepresentationException.java
+++
@@ -1,7 +0,0 @@
-package by.itechart.lastcoursetask.exception;
-
-public class InvalidDataRepresentationException extends RuntimeException {
-    public InvalidDataRepresentationException(String message) {
-        super(message);
-    }
-}
