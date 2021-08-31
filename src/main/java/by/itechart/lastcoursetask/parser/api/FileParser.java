b0a18c0a4afafe894201b0290e6bef8d5eac5e59
S1mpleKnight <become.adult77@gmail.com>
2021-08-31 11:12:23 +0300

    fix: correct parse method args

--- src/main/java/by/itechart/lastcoursetask/parser/api/FileParser.java
+++ src/main/java/by/itechart/lastcoursetask/parser/api/FileParser.java
@@ -3,12 +3,11 @@ package by.itechart.lastcoursetask.parser.api;
 import by.itechart.lastcoursetask.dto.TransactionDto;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.util.List;
 
 @Component
 public interface FileParser {
-    List<TransactionDto> parse(File file);
+    List<TransactionDto> parse(Object data);
 
     List<String> getInvalidTransactionsData();
 }
