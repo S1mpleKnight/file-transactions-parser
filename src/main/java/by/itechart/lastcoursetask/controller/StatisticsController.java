1f6b10cfdd3551542c7e8cbd61f38f2e8ab32557
S1mpleKnight <become.adult77@gmail.com>
2021-09-01 17:52:21 +0300

    fix: remove unused import from StatisticsController

--- src/main/java/by/itechart/lastcoursetask/controller/StatisticsController.java
+++ src/main/java/by/itechart/lastcoursetask/controller/StatisticsController.java
@@ -1,14 +1,13 @@
 package by.itechart.lastcoursetask.controller;
 
-import by.itechart.lastcoursetask.command.api.Command;
-import by.itechart.lastcoursetask.command.impl.CommandFactory;
+import by.itechart.lastcoursetask.command.Command;
+import by.itechart.lastcoursetask.command.CommandFactory;
 import by.itechart.lastcoursetask.dto.CommandDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
