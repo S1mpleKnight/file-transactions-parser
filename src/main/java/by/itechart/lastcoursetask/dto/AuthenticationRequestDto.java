e73320b7a6fe524a1f3b750dde24b014497c0665
S1mpleKnight <become.adult77@gmail.com>
2021-09-03 19:27:24 +0300

    fix: add nickname validation to AuthenticationRequestDto

--- src/main/java/by/itechart/lastcoursetask/dto/AuthenticationRequestDto.java
+++ src/main/java/by/itechart/lastcoursetask/dto/AuthenticationRequestDto.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Schema(description = "Authentication entity")
 @Data
 public class AuthenticationRequestDto {
     @NotBlank
+    @Pattern(regexp = "[a-zA-Z0-9@]{5,50}")
     private String nickname;
     @NotBlank
     private String password;
