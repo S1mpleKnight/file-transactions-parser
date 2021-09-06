fc3e0d9f49c16591e6a491cd18ebb151dd9027ed
S1mpleKnight <become.adult77@gmail.com>
2021-09-06 22:08:07 +0300

    fix: correct findAll method in TransactionsController(with param & no param)

--- src/main/java/by/itechart/lastcoursetask/controller/TransactionsController.java
+++ src/main/java/by/itechart/lastcoursetask/controller/TransactionsController.java
@@ -29,10 +29,14 @@ public class TransactionsController {
     private final TransactionService transactionService;
 
     @GetMapping
-    public ResponseEntity<Page<TransactionDto>> findAll(
-            @RequestParam("page") int page, @RequestParam("size") int size) {
+    public ResponseEntity<?> findAll(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size) {
         log.info("Find All");
-        return ResponseEntity.ok(transactionService.findAll(PageRequest.of(page, size)));
+        if (page != null && size != null) {
+            return ResponseEntity.ok(transactionService.findAll(PageRequest.of(page, size)));
+        }
+        return ResponseEntity.ok(transactionService.findAll());
     }
 
     @GetMapping("/{id}")
