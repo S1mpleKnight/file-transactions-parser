2256f55c9263a10f0d2749a542d8b49770f1b18b
S1mpleKnight <become.adult77@gmail.com>
2021-09-06 14:55:00 +0300

    fix: correct findAll methods in TransactionsController (pagination components)

--- src/main/java/by/itechart/lastcoursetask/controller/TransactionsController.java
+++ src/main/java/by/itechart/lastcoursetask/controller/TransactionsController.java
@@ -6,13 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,9 +29,10 @@ public class TransactionsController {
     private final TransactionService transactionService;
 
     @GetMapping
-    public ResponseEntity<Page<TransactionDto>> findAll(Pageable pageable) {
+    public ResponseEntity<Page<TransactionDto>> findAll(
+            @RequestParam("page") int page, @RequestParam("size") int size) {
         log.info("Find All");
-        return ResponseEntity.ok(transactionService.findAll(pageable));
+        return ResponseEntity.ok(transactionService.findAll(PageRequest.of(page, size)));
     }
 
     @GetMapping("/{id}")
