d0e4b120a92e33b889862a02a259ee41f9c23735
S1mpleKnight <become.adult77@gmail.com>
2021-09-07 11:30:01 +0300

    fix: correct exception handling

--- src/main/java/by/itechart/lastcoursetask/exception/handler/ExceptionHandlerController.java
+++ src/main/java/by/itechart/lastcoursetask/exception/handler/ExceptionHandlerController.java
@@ -11,28 +11,25 @@ import by.itechart.lastcoursetask.exception.RejectAccessException;
 import by.itechart.lastcoursetask.exception.RoleNotFoundException;
 import by.itechart.lastcoursetask.exception.TransactionExistException;
 import by.itechart.lastcoursetask.exception.TransactionNotFoundException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler({OperatorExistException.class, TransactionExistException.class,
-            FileNotReadException.class, InvalidFileExtensionException.class, RejectAccessException.class,
-            IllegalArgumentException.class})
+            FileNotReadException.class, InvalidFileExtensionException.class, IllegalArgumentException.class})
     public ResponseEntity<String> badRequest(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
@@ -49,9 +46,14 @@ public class ExceptionHandlerController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
-    @ExceptionHandler({JwtAuthenticationException.class})
+    @ExceptionHandler({RejectAccessException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ResponseBody
+    public String forbidden(RuntimeException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler({JwtAuthenticationException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String unauthorized(AuthenticationException exception) {
         return exception.getMessage();
     }
