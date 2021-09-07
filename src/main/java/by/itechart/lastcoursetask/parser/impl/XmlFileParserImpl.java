2e97191722463105efb2c66e21e76c1ef576c3c8
S1mpleKnight <become.adult77@gmail.com>
2021-09-07 13:01:25 +0300

    fix: change main xml file parser

--- src/main/java/by/itechart/lastcoursetask/parser/impl/XmlFileParserImpl.java
+++ src/main/java/by/itechart/lastcoursetask/parser/impl/XmlFileParserImpl.java
@@ -22,41 +22,81 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Implementation of FileParser which works with XML files
+ *
+ * @author Vanya Zelezinsky
+ * @see by.itechart.lastcoursetask.parser.impl.CsvFileParserImpl
+ * @see by.itechart.lastcoursetask.parser.api.FileParser
+ * @see by.itechart.lastcoursetask.parser.impl.FileParserFactory
+ * @since 1.0
+ */
+
 @Slf4j
 @Component("xml")
 @Scope("singleton")
 public class XmlFileParserImpl implements FileParser {
+    /**
+     * Formatter for LocalDateTime transaction value
+     */
     private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private final static String SUCCESS_TRANSACTION_STATUS = "COMPLETE";
+    /**
+     * Transaction file tag names
+     */
     private static final String TRANSACTION_TAG = "transaction";
     private static final String USER_TAG = "user";
     private static final String ID_TAG = "id";
     private static final String[] FIRST_LEVEL_TAGS = {"timestamp", "status", ID_TAG};
     private static final String[] PAYMENT_DETAILS_TAGS = {"amount", "currency"};
+    /**
+     * Regular expressions for the XML file structure validation
+     */
     private static final String DATE_TIME_REGEX = "[0-9]{4}(-[0-9]{2}){2} ([0-9]{2}:){2}[0-9]{2}";
     private static final String STATUS_REGEX = "((COMPLETE)|(FAILURE))";
     private static final String ID_REGEX = "[0-9a-z]{8}-([0-9a-z]{4}-){3}[0-9a-z]{12}";
     private static final String AMOUNT_REGEX = "[0-9 ]+";
     private static final String CURRENCY_REGEX = "[A-Z]{3}";
+    /**
+     * Storage for all exception messages, if they were occurred
+     */
     private final List<String> invalidDataMessages;
+    /**
+     * Temporary storage for the proceeding transaction
+     */
     private TransactionDto transactionDto;
 
     XmlFileParserImpl() {
         this.invalidDataMessages = new ArrayList<>();
     }
 
+    /**
+     * Takes list of exceptions
+     * @return      List of exception, which occurs while parsing incoming data
+     */
+    @Override
+    public List<String> getInvalidTransactionsData() {
+        return new ArrayList<>(invalidDataMessages);
+    }
+
+    /**
+     * Parsing incoming data from XML file to the {@code TransactionDto} list
+     * @param       stream Representation of given data
+     * @return      List of successfully parsed {@code TransactionDto} objects
+     * @see         TransactionDto
+     */
     @Override
     public List<TransactionDto> parse(InputStream stream, String filename) {
         log.info("Parsing XML file");
         this.invalidDataMessages.clear();
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        return getTransactions(stream, filename, factory);
+        return getParsedTransactions(stream, filename, factory);
     }
 
-    private List<TransactionDto> getTransactions(InputStream stream, String filename, DocumentBuilderFactory factory) {
+    private List<TransactionDto> getParsedTransactions(InputStream stream, String filename, DocumentBuilderFactory factory) {
         try {
             NodeList list = getNodeList(stream, factory);
-            return getTransactions(list, filename);
+            return getTransactionsFromNodeList(list, filename);
         } catch (ParserConfigurationException | IOException | SAXException e) {
             log.error(e.getMessage());
             return Collections.emptyList();
@@ -70,7 +110,7 @@ public class XmlFileParserImpl implements FileParser {
         return document.getElementsByTagName(TRANSACTION_TAG);
     }
 
-    private List<TransactionDto> getTransactions(NodeList list, String filename) {
+    private List<TransactionDto> getTransactionsFromNodeList(NodeList list, String filename) {
         List<TransactionDto> transactions = new ArrayList<>();
         for (int i = 0; i < list.getLength(); i++) {
             getDataFromTransaction((Element) list.item(i), transactions, i, filename);
@@ -147,10 +187,5 @@ public class XmlFileParserImpl implements FileParser {
             case "currency" -> CURRENCY_REGEX;
             default -> throw new IllegalArgumentException("Illegal tag name");
         };
-    }
-
-    @Override
-    public List<String> getInvalidTransactionsData() {
-        return new ArrayList<>(invalidDataMessages);
     }
 }
